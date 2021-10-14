0184b627776c06ff4e57b60d70202df62a7d9166
RaheemWilson <wilsonzackery16@gmail.com>
2021-10-14 13:29:19 -0500

    Added js functionality to newsletter button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+
+window.addEventListener("load", () => {
+    let form = document.getElementsByTagName("FORM")[0]
+    let emailField = document.getElementById("email")
+    let message = document.getElementsByClassName("message")[0]
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault()
+        if(emailField.value == ""){
+            message.innerHTML = "Please enter valid email address"
+        }else{
+            message.innerHTML = `Thank you! Your email address ${emailField.value} has been added to our mailing list!`
+        }
+    })
+})
